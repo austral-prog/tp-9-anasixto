660e9800aa3ab69a846fc1b73a814a6f9e685e53
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-31 11:53:52 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,25 +1,60 @@
-
 def create_inventory(items):
+    #Creo un diccionario vacío  
+    inventory = {}
+    #Recorrer los elementos de la lista
+    for item in items
+        #Si el artículo ya está en el diccionario, incrementar su valor
+        if item in inventory:
+            inventory[item] += 1
+        #Si no lo está dejar el valor en uno
+        else:
+            inventory[item] = 1
     
-    return {}
+    return inventory
 
 
 def add_items(inventory, items):
+
+    if item in items:
+        inventory[item] += 1
+
+    else:
+        inventory[item] = 1
    
-    return {}
+    return inventory
 
 
 def decrement_items(inventory, items):
 
-    return {}
+    inventory = {}
+
+    for item in items
+
+        if item in inventory:
+            inventory[item] -= 1
+
+        else:
+            inventory[item] = 1
+    
+    return inventory
 
 
 def remove_item(inventory, item):
+
+    inventory.pop(item, None)
     
-    return {}
+    return inventory
 
 
 def list_inventory(inventory):
-    
-    return {}
+
+    for item in list_inventory.items():
+
+        key = item[0]
+        value = item[1]
+
+        print(key)
+        print(value)
+        
+    return inventory
 
